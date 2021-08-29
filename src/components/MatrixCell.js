07ba959c8156842cef73f030e7b3263e3cd05b0d
jimzrt <james.tophoven@gmail.com>
2021-08-29 14:47:35 +0200

    formatting + cleanup

--- src/components/MatrixCell.js
+++ src/components/MatrixCell.js
@@ -1,42 +1,43 @@
 import React, { useState } from "react";
 
-const MatrixCell = ({
-    initialValue,
-    onUpdate,
-    ...props
-}) => {
-    const [isEditing, setEditing] = useState(false);
-    const [value, setValue] = useState(initialValue)
+const MatrixCell = ({ initialValue, onUpdate, ...props }) => {
+  const [isEditing, setEditing] = useState(false);
+  const [value, setValue] = useState(initialValue);
 
-    const makeEditable = () => {
-        setEditing(true)
-    };
-    const changeValue = (event) => {
-        let newValue = parseInt(event.target.value)
-        if (isNaN(newValue)) {
-            newValue = 0;
-        }
-        setValue(newValue)
-    };
-    const updateValue = () => {
-        setEditing(false);
-        if (initialValue !== value) {
-            onUpdate(value)
-        }
+  const changeValue = (event) => {
+    let newValue = parseInt(event.target.value);
+    if (isNaN(newValue)) {
+      newValue = 0;
     }
-    return (<section {...props} > {
-        isEditing ? (< input type="text"
-            autoFocus value={value}
-            onChange={changeValue}
-            onBlur={updateValue}
-            onKeyPress={(event) => {
-                if (event.key === "Enter") {
-                    updateValue()
-                }
-            }}
-            style={{ 'width': `calc(2vmin*${value.toString().length})` }}
-        />) : (<div onClick={makeEditable}>{value}</div >)
-    } </section>)
-}
+    setValue(newValue);
+  };
+  const updateValue = () => {
+    setEditing(false);
+    if (initialValue !== value) {
+      onUpdate(value);
+    }
+  };
+  return (
+    <section {...props}>
+      {isEditing ? (
+        <input
+          type="text"
+          autoFocus
+          value={value}
+          onChange={changeValue}
+          onBlur={updateValue}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              updateValue();
+            }
+          }}
+          style={{ width: `calc(2vmin*${value.toString().length})` }}
+        />
+      ) : (
+        <div onClick={() => setEditing(true)}>{value}</div>
+      )}
+    </section>
+  );
+};
 
 export default MatrixCell;
